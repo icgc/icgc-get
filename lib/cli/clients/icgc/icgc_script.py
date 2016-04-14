0bdff2addf3367261b79589af6970b4144443368
GavinMWilson <Gavin.M.Wilson@oicr.on.ca>
2016-04-14 16:59:40 -0400

    added updated icgc script which wasn't added to last commit

--- lib/cli/clients/icgc/icgc_script.py
+++ lib/cli/clients/icgc/icgc_script.py
@@ -1,26 +1,15 @@
-import subprocess
-import logging
 import os
+from ..run_command import run_command
 
 
 def icgc_call(object_id, token, tool_path, output):
-    logger = logging.getLogger('__log__')
 
     os.environ['ACCESSTOKEN'] = token
     call_args = [tool_path, 'download', '--object-id', object_id, '--output-dir', output]
-    logger.debug(call_args)
-    call = subprocess.Popen(call_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, error = call.communicate()
-    logger.info(output)
-    logger.warning(error)
+    run_command(call_args)
 
 
 def icgc_manifest_call(manifest, token, tool_path, output):
-    logger = logging.getLogger('__log__')
     os.environ['ACCESSTOKEN'] = token
     call_args = {tool_path, '--manifest', manifest,  '--output-dir', output}
-    logger.debug(call_args)
-    call = subprocess.Popen(call_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, error = call.communicate()
-    logger.info(output)
-    logger.warning(error)
+    run_command(call_args)
